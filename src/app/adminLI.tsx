636fc12135c36d4618aa579c6295f18b96a99a42
cpmullane <cpmullane@wpi.edu>
2024-11-19 17:34:49 -0500

    started implementing log in admin function

--- src/app/adminLI.tsx
+++ src/app/adminLI.tsx
@@ -1,19 +1,21 @@
 'use client'                                              // directive to clarify client-side. Place at top of ALL .tsx files
 import React from 'react'
+import {Admin} from '../admin';
 
 export default function Home() {
     // initial instantiation for admin log in page
     const [redraw, forceRedraw] = React.useState(0)
 
     // helper function that forces React app to redraw whenever this is called.
-  function andRefreshDisplay() {
+    function andRefreshDisplay() {
     forceRedraw(redraw + 1)
   }
 
   // below is where the GUI for the admin log in page is drawn
   return (
     <div>
-      <button className="admin log in" onClick={(e) => /*brings user to admin home page*/} >Log In</button>
+      // how do i input something into a function that was typed in by the user?
+      <button className="admin log in" onClick={(e) => Admin.logIn(email, password)} >Log In</button>
 
       // figure out how to do textbox
       <label className="email">{"Email:"}</label>
