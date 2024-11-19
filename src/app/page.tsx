5a104d6f2ff94174a7c245ac0ac78f9f84dc3d83
cpmullane <cpmullane@wpi.edu>
2024-11-19 14:23:51 -0500

    started implementing home page design w/ jess

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,28 @@
-'use client'                              // directive to clarify client-side. Place at top of ALL .tsx files
+'use client'                                              // directive to clarify client-side. Place at top of ALL .tsx files
 import React from 'react'
 
 export default function Home() {
+    // initial instantiation
+    const [redraw, forceRedraw] = React.useState(0)
+
+    // helper function that forces React app to redraw whenever this is called.
+  function andRefreshDisplay() {
+    forceRedraw(redraw + 1)
+  }
+
+  // below is where the GUI is drawn
   return (
     <div>
-      <div className="board">
-        <div className="button-container">
-          <button data-testid="0,0" className={css(0,0)} onClick={() => handleClick(0,0)}>{model.contents(0,0)}</button>
-          <button data-testid="0,1" className={css(0,1)} onClick={() => handleClick(0,1)}>{model.contents(0,1)}</button>
-          <button data-testid="0,2" className={css(0,2)} onClick={() => handleClick(0,2)}>{model.contents(0,2)}</button>
-          <button data-testid="0,3" className={css(0,3)} onClick={() => handleClick(0,3)}>{model.contents(0,3)}</button>
-          <button data-testid="0,4" className={css(0,4)} onClick={() => handleClick(0,4)}>{model.contents(0,4)}</button>
-        </div>
-      </div>
+      <button className="admin log in" onClick={(e) => /*brings user to admin log in page*/} >Admin Log In</button>
+      <button className="manager log in" onClick={(e) => /*brings user to manager log in page*/} >Manager Log In</button>
+      <button className="confirm res" onClick={(e) => /*confirms reservation when given a confirmation code*/} >Enter</button>
+
+      // figure out how to do textbox
+      <label className="date">{"Date:"}</label>
+      <label className="time">{"Start Time:"}</label>
+      <label className="message">{"Already have a reservation? Find details here!"}</label>
+      <label className="confirm">{"Confirmation Code"}</label>
+
     </div>
   )
 }
