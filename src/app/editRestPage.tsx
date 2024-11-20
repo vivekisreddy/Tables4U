a3854c6f78bab8850f98e6b852162c943125e8aa
j411wong <jmwong@wpi.edu>
2024-11-20 13:50:37 -0500

    started Edit Restaurant Page.tsx file

--- src/app/editRestPage.tsx
+++ src/app/editRestPage.tsx
@@ -10,35 +10,32 @@ export default function homePage() {
     forceRedraw(redraw + 1)
   }
 
-  function adminLog() {
-    andRefreshDisplay()
-  }
-
-  function managerLog() {
-    andRefreshDisplay()
-  }
-
-  function confirmRes() {
-    andRefreshDisplay()
-  }
 
   // below is where the GUI for the landing home page is drawn
   return (
     <div>
+      
       <div className = "topInfo">
-        <button className="admin log in" onClick={() => adminLog()}>{"Admin Log In"}</button>
-        <button className="manager log in" onClick={() => managerLog()}>{"Manager Log In"}</button>
-        <button className="confirm res" onClick={() => confirmRes()}>{"Enter<"}</button>
+         <label className="RestaurantName">{"Restaurant Name:"}</label>
+         <text className="textField">{"Enter Name Here"}</text>
+         <label className="Address">{"Restaurant Address:"}</label>
+         <text className="textField">{"Enter Address Here"}</text>
+         <div className = "Weekly Hours">
+             <label className="date">{"Date:"}</label>
+             <text className="textField">{"Enter Date Here"}</text>
+         </div>
+         <div className = "TablesAndSeats">
+             <label className="date">{"Date:"}</label>
+             <text className="textField">{"Enter Date Here"}</text>
+         </div>
       </div>
       
-      <label className="date">{"Date:"}</label>
-      <text className="textField">{"Enter Date Here"}</text>
       <label className="time">{"Start Time:"}</label>
       <text className="textField">{"Enter Time Here"}</text>
+      
       <label className="confirm">{"Confirmation Code:"}</label>
       <text className="textField">{"Enter Confirmation Code Here"}</text>
 
-      <label className="message">{"Already have a reservation? Find details here!"}</label>
     </div>
   )
 }
