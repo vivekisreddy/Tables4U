90bdaf95b66d7b35fab6c9204535affe63b64a42
j411wong <jmwong@wpi.edu>
2024-11-25 00:51:59 -0500

    edited editRes in createRes and managerLogIn

--- src/app/createRes/page.tsx
+++ src/app/createRes/page.tsx
@@ -1,19 +1,19 @@
 'use client'                                              // directive to clarify client-side. Place at top of ALL .tsx files
 
 import React from "react";
-import { RestaurantController } from "../restaurantController";
+
 
 export default function Home() {
-    const controller = new RestaurantController();
 
     const[redraw, forceRedraw] = React.useState(0)
-    const[resName, setResName] = React.useState('')
-    const[resAddress, setResAddress] = React.useState('')
-    const[resNumTables, setResNumTables] = React.useState(0)
+    const[name, setResName] = React.useState('')
+    const[address, setResAddress] = React.useState('')
+    const[restaurantID, IDreference] = React.useState('')
+    const[tables, setResNumTables] = React.useState(0);
+    const[isActive, setIsActive] = React.useState(false)
     const[resSeatsPerTable, setResSeatsPerTable] = React.useState<number[]>([]);
-    const[message, setMessage] = React.useState('');
-    const[resOpenTime, setResOpenTime] = React.useState(0);
-    const[resCloseTime, setResCloseTime] = React.useState(0);
+    const[openTime, setResOpenTime] = React.useState(0);
+    const[closeTime, setResCloseTime] = React.useState(0);
 
 
 
@@ -29,21 +29,57 @@ export default function Home() {
 };
 
   const handleCreateRestaurant = () => {
-    const result = controller.createRestaurant(resName, resAddress, resOpenTime, resCloseTime, resNumTables, resSeatsPerTable);
-    setMessage(result); 
-    setResName('');
-    setResAddress('');
-    setResNumTables(0);
-    setResOpenTime(0);
-    setResOpenTime(0);
-    setResSeatsPerTable([]);
-    // TO DO: create restaurant lambda functions
-    // display credentials then log in button to bring user back to manager log in page
+    
 };
 
-const handleEditRestaurant = () => {
-    const result = controller.editRestaurant(resName, resAddress, resOpenTime, resCloseTime, resNumTables, resSeatsPerTable);
-    setMessage(result);
+const editRes = (name:string, address:string, restaurantID:string, isActive:Boolean, openTime:number, closeTime:number, tables:number, seats:number[]) => {
+    let payload = {
+        "name": name, "address": address, "restaurantID": restaurantID, "isActive":isActive, "openTime":openTime, "closeTime":closeTime, "tables":tables, "seats":seats
+      }
+      fetch("https://hxb5sgcwlguyaco5uaidn23l3a0nkajp.lambda-url.us-east-1.on.aws/", {
+        method: "POST",
+        body: JSON.stringify(payload)
+      })
+}
+
+const handleEditRestaurant = (and) => {
+    and.preventDefault()
+    if (name == '') {
+      alert("Please enter your name")
+    }
+    if (address == '') {
+      alert("Please enter your restaurant's address")
+    }
+    if (isActive == true) {
+        alert("Cannot Edit - Restaurant is already Active")
+      }
+    if (restaurantID == '') {
+    alert("Restaurant cannot be found")
+    }
+    if (openTime == null) {
+    alert("Please enter an opening time")
+    }
+    if (closeTime == null) {
+    alert("Please enter a closing time")
+    }
+    if (tables == 0) {
+        alert("Please enter more tables")
+    }
+    if (resSeatsPerTable.length == 0) {
+        alert("Please enter more table seats")
+    }
+
+    console.log('Restaurant Name:', name)
+    console.log('Restaurant Address:', address)
+    console.log('Restaurant ID:', restaurantID)
+    console.log('Restaurant isActive:', isActive)
+    console.log('Restaurant openTime:', openTime)
+    console.log('Restaurant closeTime:', closeTime)
+    console.log('Tables:', tables)
+    console.log('Seats Per Table:', resSeatsPerTable)
+    editRes(name, address, restaurantID, isActive, openTime, closeTime, tables, resSeatsPerTable)
+
+
 };
 
 
@@ -54,7 +90,7 @@ return (
             Restaurant Name:
             <input
                 type="text"
-                value={resName}
+                value={name}
                 onChange={(e) => setResName(e.target.value)}
                 className="input"
             />
@@ -63,7 +99,7 @@ return (
             Restaurant Address:
             <input
                 type="text"
-                value={resAddress}
+                value={address}
                 onChange={(e) => setResAddress(e.target.value)}
                 className="input"
             />
@@ -72,7 +108,7 @@ return (
             Open Time:
             <input
                 type="number"
-                value={resOpenTime}
+                value={openTime}
                 onChange={(e) => setResOpenTime(Number(e.target.value))}
                 className="input"
             />
@@ -81,25 +117,7 @@ return (
             Close Time:
             <input
                 type="number"  
-                value={resCloseTime}
-                onChange={(e) => setResCloseTime(Number(e.target.value))}  // Ensure it's converted to a number
-                className="input"
-            />
-        </label>
-        <label className="label">
-            Open Time:
-            <input
-                type="number"
-                value={resOpenTime}
-                onChange={(e) => setResOpenTime(Number(e.target.value))}
-                className="input"
-            />
-        </label>
-        <label className="label">
-            Close Time:
-            <input
-                type="number"  
-                value={resCloseTime}
+                value={closeTime}
                 onChange={(e) => setResCloseTime(Number(e.target.value))}  // Ensure it's converted to a number
                 className="input"
             />
@@ -108,13 +126,13 @@ return (
             Number of Tables:
             <input
                 type="number"
-                value={resNumTables}
+                value={tables}
                 onChange={(e) => setResNumTables(Number(e.target.value))}
                 className="input"
             />
         </label>
 
-        {[...Array(resNumTables)].map((_, index) => (
+        {[...Array(tables)].map((_, index) => (
                 <div key={index}>
                     <label className="label">
                         Seats at Table {index + 1}:
@@ -127,14 +145,16 @@ return (
                     </label>
                 </div>
             ))}
-
-        <button onClick={handleCreateRestaurant} className="button-createRes">
-            Create Restaurant
-        </button>
-        <button onClick={handleEditRestaurant} className="button-editRes">
-            Edit Restaurant
-        </button>
-        {message && <p className="message">{message}</p>}
+        <div className="container">
+            <button onClick={handleCreateRestaurant} className="button-createRes">
+                Create Restaurant
+            </button>
+        </div>
+        <div className="container">
+            <button onClick={handleEditRestaurant} className="button-editRes">
+                Complete Edits
+            </button>
+        </div>
     </div>
 );
 }
--- src/app/managerLogIn/page.tsx
+++ src/app/managerLogIn/page.tsx
@@ -6,27 +6,43 @@ export default function Home() {
     // initial instantiation for admin log in page
     const [redraw, forceRedraw] = React.useState(0)
 
-    const [name, setName] = React.useState('')
-    const [pin, setPin] = React.useState('')
+    const [manager, setManager] = React.useState('')
+    const [resID, setID] = React.useState('')
 
     // helper function that forces React app to redraw whenever this is called.
     function andRefreshDisplay() {
     forceRedraw(redraw + 1)
   }
 
+  function managerLogIn(manager:String, resID:String) {
+    let payload = {
+      "manager": manager,
+      "resID": resID
+    }
+    fetch("https://thvplief2n4e6qwpg6nl5x7fti0mdklc.lambda-url.us-east-1.on.aws/", {
+      method: "GET",
+      body: JSON.stringify(payload)
+    })
+  }
+
   const handleLogIn = (and) => {
     and.preventDefault()
-    // TO DO: manager log in lambda function
-    console.log('Manager Name:', name)
-    console.log('Restaurant Pin Code:', pin)
-    managerLogIn(name, pin)
-    andRefreshDisplay()
+    if (manager == '') {
+      alert("Please enter your name")
+    }
+    if (resID == '') {
+      alert("Please enter your restaurant's ID")
+    }
+    console.log('Manager Name:', manager)
+    console.log('Restaurant ID:', resID)
+    managerLogIn(manager, resID)
   }
 
   function createAccount() {
-    window.location.replace("/createRes")
+    window.location.replace("/managerCreateAccount")
     andRefreshDisplay()
   }
+
 
   // below is where the GUI for the admin log in page is drawn
   return (
@@ -35,16 +51,16 @@ export default function Home() {
 
       <form className="handleLogIn" onSubmit={handleLogIn}>
         <label className="label" htmlFor="name">Restaurant Name:</label>
-        <input type="text" id="name" name="name" value={name} onChange={(and) => setName(and.target.value)}/>
+        <input type="text" id="name" name="name" value={manager} onChange={(and) => setManager(and.target.value)}/>
         <br></br>
         <br></br>
         <label className="label" htmlFor="pin">Pin Code:</label>
-        <input type="text" id="pin" name="pin" value={pin} onChange={(and) => setPin(and.target.value)}/>
+        <input type="text" id="pin" name="pin" value={resID} onChange={(and) => setID(and.target.value)}/>
         <button type="submit" className="managerLogIn">Log In</button>
       </form>
 
       <label className="account">{"Don't have an account? Create one now!"}</label>
-      <button className="createManagerLogIn" onClick={(e) => createAccount()}>Create Restaurant</button>
+      <button className="createManagerLogIn" onClick={() => createAccount()}>Create Restaurant</button>
     </div>
   )
 }
