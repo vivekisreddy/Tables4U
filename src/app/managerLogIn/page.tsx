42523e68b683a5ba0e9d5bd642200d45f62a1172
Jahnavi Prudhvi <jahnaviprudhivi2004@gmail.com>
2024-11-25 01:33:40 -0500

    Jahnavi and Vivek added React and API gateway conneciton for createRestaurant and activateRestaurant. Still need to fix some issue tho

--- src/app/managerLogIn/page.tsx
+++ src/app/managerLogIn/page.tsx
@@ -1,52 +1,52 @@
-// 'use client'                                              // directive to clarify client-side. Place at top of ALL .tsx files
-// import React from 'react'
+'use client'                                              // directive to clarify client-side. Place at top of ALL .tsx files
+import React from 'react'
 
-// //i commented out everything in rLoginController page, that's why it's throwing an error.
-// import {managerLogIn} from '../../rLoginController'
+//i commented out everything in rLoginController page, that's why it's throwing an error.
+//import {managerLogIn} from '../../rLoginController'
 
-// export default function Home() {
-//     // initial instantiation for admin log in page
-//     const [redraw, forceRedraw] = React.useState(0)
+export default function Home() {
+    // initial instantiation for admin log in page
+    const [redraw, forceRedraw] = React.useState(0)
 
-//     const [name, setName] = React.useState('')
-//     const [pin, setPin] = React.useState('')
+    const [name, setName] = React.useState('')
+    const [pin, setPin] = React.useState('')
 
-//     // helper function that forces React app to redraw whenever this is called.
-//     function andRefreshDisplay() {
-//     forceRedraw(redraw + 1)
-//   }
+    // helper function that forces React app to redraw whenever this is called.
+    function andRefreshDisplay() {
+    forceRedraw(redraw + 1)
+  }
 
-//   const handleLogIn = (and) => {
-//     and.preventDefault()
-//     // TO DO: manager log in lambda function
-//     console.log('Manager Name:', name)
-//     console.log('Restaurant Pin Code:', pin)
-//     managerLogIn(name, pin)
-//     andRefreshDisplay()
-//   }
+  const handleLogIn = (and) => {
+    and.preventDefault()
+    // TO DO: manager log in lambda function
+    console.log('Manager Name:', name)
+    console.log('Restaurant Pin Code:', pin)
+    //managerLogIn(name, pin)
+    andRefreshDisplay()
+  }
 
-//   function createAccount() {
-//     window.location.replace("/createRes")
-//     andRefreshDisplay()
-//   }
+  function createAccount() {
+    window.location.replace("/createRes")
+    andRefreshDisplay()
+  }
 
-//   // below is where the GUI for the admin log in page is drawn
-//   return (
-//     <div>
-//       <label className="managerLogInMessage">{"Manager Log In"}</label>
+  // below is where the GUI for the admin log in page is drawn
+  return (
+    <div>
+      <label className="managerLogInMessage">{"Manager Log In"}</label>
 
-//       <form className="handleLogIn" onSubmit={handleLogIn}>
-//         <label className="label" htmlFor="name">Restaurant Name:</label>
-//         <input type="text" id="name" name="name" value={name} onChange={(and) => setName(and.target.value)}/>
-//         <br></br>
-//         <br></br>
-//         <label className="label" htmlFor="pin">Pin Code:</label>
-//         <input type="text" id="pin" name="pin" value={pin} onChange={(and) => setPin(and.target.value)}/>
-//         <button type="submit" className="managerLogIn">Log In</button>
-//       </form>
+      <form className="handleLogIn" onSubmit={handleLogIn}>
+        <label className="label" htmlFor="name">Restaurant Name:</label>
+        <input type="text" id="name" name="name" value={name} onChange={(and) => setName(and.target.value)}/>
+        <br></br>
+        <br></br>
+        <label className="label" htmlFor="pin">Pin Code:</label>
+        <input type="text" id="pin" name="pin" value={pin} onChange={(and) => setPin(and.target.value)}/>
+        <button type="submit" className="managerLogIn">Log In</button>
+      </form>
 
-//       <label className="account">{"Don't have an account? Create one now!"}</label>
-//       <button className="createManagerLogIn" onClick={(e) => createAccount()}>Create Restaurant</button>
-//     </div>
-//   )
-// }
+      <label className="account">{"Don't have an account? Create one now!"}</label>
+      <button className="createManagerLogIn" onClick={(e) => createAccount()}>Create Restaurant</button>
+    </div>
+  )
+}
