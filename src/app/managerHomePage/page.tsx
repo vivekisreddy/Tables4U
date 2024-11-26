009a46c4f42e746f3ff218d48de7fa95a43d544f
j411wong <jmwong@wpi.edu>
2024-11-26 15:55:29 -0500

    need help with viewing Restaurant info on page

--- src/app/managerHomePage/page.tsx
+++ src/app/managerHomePage/page.tsx
@@ -5,14 +5,40 @@ import axios from 'axios';
 export default function Home() {
     // initial instantiation for admin home page
     const [redraw, forceRedraw] = React.useState(0)
+    const [resName, setResName] = useState('');
+    const [resAddress, setResAddress] = useState('');
+    const [resNumTables, setResNumTables] = useState(0);
+    const [resSeatsPerTable, setResSeatsPerTable] = useState<number[]>([]);
+    const [resOpenTime, setResOpenTime] = useState(0);
+    const [resCloseTime, setResCloseTime] = useState(0);
+    const [resClosedDays, setResClosedDays] = useState<string[]>([]);
+    const [resID, setRestaurantID] = useState('');  // Store the restaurantID
+    const [message, setMessage] = useState('');
 
-    const [message, setMessage] = useState('')
-    const [resID, setResID] = useState('')
+    let restaurantInfo : [];
 
     // helper function that forces React app to redraw whenever this is called.
     function andRefreshDisplay() {
     forceRedraw(redraw + 1)
-  }
+    }
+
+    const instance = axios.create({
+      baseURL: 'https://cy11llfdh5.execute-api.us-east-1.amazonaws.com/Initial'
+    });
+
+    function viewDetails() {
+      instance.post('/viewResDetails', {"name": resName, "address": resAddress, "openTime":resOpenTime, "closeTime":resCloseTime, "tables":resNumTables, "seats":resSeatsPerTable,})
+      .then(function (response) {
+        console.log("raw response:", response)
+        let status = response.data.statusCode
+        let result = response.data.body
+        restaurantInfo = result;
+        setResName(result[0])
+        setResAddress(result[1])
+        setResOpenTime(result[2])
+        setResCloseTime(result[3])
+      })
+    }
 
   const handleActivateRestaurant = async () => {
     if (!resID) {
@@ -46,7 +72,7 @@ export default function Home() {
 };
 
   function managerAccount() {
-    // displays account information
+    viewDetails()
     // log out button
     andRefreshDisplay()
   }
@@ -66,10 +92,19 @@ export default function Home() {
   return (
     <div>
       <button className="managerAccountButton" onClick={(e) => managerAccount()} >Account Information</button>
-
+      <div className = "container">
+        <label className="label" htmlFor="Restaurant Name:">Restaurant Name:</label>
+        <label className="label" htmlFor="resName">{resName}</label>
+        <label className="label" htmlFor="Restaurant Address">Restaurant Address:</label>
+        <label className="label" htmlFor="resAddress">{resAddress}</label>
+        <label className="label" htmlFor="Restaurant Open Time">Restaurant Open Time:</label>
+        <label className="label" htmlFor="resOpenTime">{resOpenTime}</label>
+        <label className="label" htmlFor="Restaurant Close Time">Restaurant Close Time:</label>
+        <label className="label" htmlFor="resCloseTime">{resCloseTime}</label>
+      </div>
       <form className="handleActivateRestaurant" onSubmit={activateRestaurant}>
         <label className="label" htmlFor="resID">Restaurant ID:</label>
-        <input type="text" style={{ color: 'black' }} id="resID" name="resID" value={resID} onChange={(and) => setResID(and.target.value)}/>
+        <input type="text" style={{ color: 'black' }} id="resID" name="resID" value={resID} onChange={(and) => setRestaurantID(and.target.value)}/>
         <button type="submit" className="activateRestaurantButton">Activate Restaurant</button>
       </form>
 
