ae619de8ab767d3c6f80a09b61eb9537517fda76
cpmullane <cpmullane@wpi.edu>
2024-12-02 16:54:07 -0500

    front-end for delete reservation (c) works

--- src/app/consumerViewReservation/page.tsx
+++ src/app/consumerViewReservation/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     const [redraw, forceRedraw] = React.useState(0)
 
     const [code, setCode] = useState('');
+    const [code1, setCode1] = useState('');
 
     const instance = axios.create({
       baseURL: 'https://cy11llfdh5.execute-api.us-east-1.amazonaws.com/Initial'
@@ -43,6 +44,33 @@ export default function Home() {
     }
   }
 
+  function deleteRes() {
+    if (code1) {
+      
+      // Access the REST-based API and in response (on a 200 or 400) process.
+      instance.post('', {"reservationID":code1})
+      .then(function (response) {
+        console.log("raw response:", response)
+        let status = response.data.statusCode
+        let result = response.data.body
+
+        console.log("response status:", status)
+
+        if (status == 200) {
+          alert("Successfully deleted reservation.")
+          window.location.replace("/page")
+          andRefreshDisplay()
+        } else {
+          alert("Error deleting reservation: " + result)
+        }
+      })
+      .catch(function (error) {
+        console.log(error)
+        alert("An unexpected error occured. Please try again.")
+      })
+    }
+  }
+
   const handleConfirm = (and:any) => {
     and.preventDefault();
     if (code == '') {
@@ -50,6 +78,17 @@ export default function Home() {
     }
     else {
       confirmRes()
+    }
+    andRefreshDisplay()
+  }
+
+  const handleDelete = (and:any) => {
+    and.preventDefault();
+    if (code1 == '') {
+      alert("Please enter your confirmation code to delete your reservation.")
+    }
+    else {
+      deleteRes()
     }
     andRefreshDisplay()
   }
@@ -63,6 +102,14 @@ export default function Home() {
         <label className="label" htmlFor="code">Confirmation Code:</label>
         <input type="text" style={{ color: 'black' }} id="code" name="code" value={code} onChange={(and) => setCode(and.target.value)}/>
         <button type="submit" className="enter">Enter</button>
+      </form>
+
+      <label className="deleteResMessage">{"Or delete your reservation here:"}</label>
+
+      <form className="handleDelete" onSubmit={handleDelete}>
+        <label className="label" htmlFor="code1">Confirmation Code:</label>
+        <input type="text" style={{ color: 'black' }} id="code1" name="code1" value={code1} onChange={(and) => setCode1(and.target.value)}/>
+        <button type="submit" className="deleteResButton">Delete</button>
       </form>
     </div>
   )
