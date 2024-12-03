197bff88ba61a611087fe7f881e278f919e5141b
cpmullane <cpmullane@wpi.edu>
2024-12-03 16:20:25 -0500

    started generate availability report (a) front-end

--- src/app/generateReport/page.tsx
+++ src/app/generateReport/page.tsx
@@ -6,8 +6,9 @@ export default function Home() {
     // initial instantiation for admin log in page
     const [redraw, forceRedraw] = React.useState(0)
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [restaurant, setRestaurant] = useState('')
+    const [start, setStart] = useState('')
+    const [end, setEnd] = useState('')
 
     const instance = axios.create({
       baseURL: 'https://cy11llfdh5.execute-api.us-east-1.amazonaws.com/Initial'
@@ -18,61 +19,66 @@ export default function Home() {
     forceRedraw(redraw + 1)
   }
 
-  function adminCreateAccount() {
-    if (email && password) {
-      
-      // Access the REST-based API and in response (on a 200 or 400) process.
-      instance.post('/adminCreateAccount', {"adminID":email, "password":password})
-      .then(function (response) {
-        console.log("raw response:", response)
-        let status = response.data.statusCode
-        let result = response.data.body
+  function generateReport() {
+    if (restaurant && start && end) {
+        // Access the REST-based API and in response (on a 200 or 400) process.
+        instance.post('/adminAvailabilityReport', { "resID":restaurant, "startDate":start, "endDate": end })
+            .then(function (response) {
+                console.log("raw response:", response);
+                let status = response.data.statusCode;
+                let result = response.data.body;
 
-        console.log("response status:", status)
-
-        if (status == 200) {
-          console.log("response status:", status)
-          console.log("Admin account successfully created")
-          window.location.replace('/adminLogIn')
-          andRefreshDisplay()
-        } else {
-          console.log("Error creating admin account:", result)
-          alert("Error creating admin account: " + result)
-        }
-      })
-      .catch(function (error) {
-        console.log(error)
-        alert("An unexpected error occured.");
-      })
+                if (status === 200) {
+                  // TO DO: display report information
+                    andRefreshDisplay();
+                } else {
+                    // console.log("Error generating availability report:", result);
+                    alert("Error generating availability report: " + result);
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+                alert("An unexpected error occurred.");
+            });
     }
+}
+
+  const handleGenerate = (and:any) => {
+    and.preventDefault();
+    if (restaurant == '') {
+      alert("Please enter the restaurant ID.")
+    }
+    // TO DO: make sure start date is not in the future
+    if (start == '') {
+      alert("Please input a start date for the report.")
+    }
+    // TO DO: make sure end date is not in the future & not before the start date
+    if (end == '') {
+      alert("Please input an end date for the report.")
+    }
+    else {
+      generateReport()
+    }
+    andRefreshDisplay()
   }
 
-  const handleCreate = (and:any) => {
-    and.preventDefault()
-    if (email == '') {
-      alert("Please enter an email address")
-    }
-    if (password == '') {
-      alert("Please create a password")
-    }
-    console.log('Admine Email:', email)
-    console.log('Admin Password:', password)
-    adminCreateAccount()
-  }
-
-  // below is where the GUI for the admin log in page is drawn
+  // below is where the GUI for the admin generate availability report is drawn
   return (
     <div>
-      <label className="adminCreateAccountMessage">{"Create an Admin Account:"}</label>
+      <label className="generateReportMessage">{"Generate Availability Report:"}</label>
 
-      <form className="handleCreate" onSubmit={handleCreate}>
-        <label className="label" htmlFor="email">Email:</label>
-        <input type="text" style={{ color: 'black' }} id="email" name="email" value={email} onChange={(and) => setEmail(and.target.value)}/>
+      <form className="handleGenerate" onSubmit={handleGenerate}>
+        <label className="label" htmlFor="restaurant">Restaurant ID:</label>
+        <input type="text" style={{ color: 'black' }} id="restaurant" name="restaurant" value={restaurant} onChange={(and) => setRestaurant(and.target.value)}/>
         <br></br>
         <br></br>
-        <label className="label" htmlFor="password">Password:</label>
-        <input type="text" style={{ color: 'black' }} id="password" name="password" value={password} onChange={(and) => setPassword(and.target.value)}/>
-        <button type="submit" className="createAdminAccount">Create</button>
+        <label className="label" htmlFor="start">Start Date:</label>
+        <input type="text" style={{ color: 'black' }} id="start" name="start" value={start} onChange={(and) => setStart(and.target.value)}/>
+        <br></br>
+        <br></br>
+        <label className="label" htmlFor="end">End Date:</label>
+        <input type="text" style={{ color: 'black' }} id="end" name="end" value={end} onChange={(and) => setEnd(and.target.value)}/>
+        <button type="submit" className="generateReportButton">Generate</button>
       </form>
     </div>
   )
