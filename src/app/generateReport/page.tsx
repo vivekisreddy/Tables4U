3a48b810dad23dad00f4aa41292496e467cc5a23
cpmullane <cpmullane@wpi.edu>
2024-12-04 14:56:53 -0500

    boundaries for start and end dates for availability report

--- src/app/generateReport/page.tsx
+++ src/app/generateReport/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
     const [redraw, forceRedraw] = React.useState(0)
 
     const currentDate = new Date();
-    const formattedCurrentDate = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`
+    const currentDateString = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`
 
     const [restaurant, setRestaurant] = useState('')
-    const [start, setStart] = useState('')
-    const [end, setEnd] = useState('')
+    const [startString, setStartString] = useState('')
+    const [endString, setEndString] = useState('')
 
     const instance = axios.create({
       baseURL: 'https://cy11llfdh5.execute-api.us-east-1.amazonaws.com/Initial'
@@ -23,9 +23,9 @@ export default function Home() {
   }
 
   function generateReport() {
-    if (restaurant && start && end) {
+    if (restaurant && startString && endString) {
         // Access the REST-based API and in response (on a 200 or 400) process.
-        instance.post('/adminAvailabilityReport', { "resID":restaurant, "startDate":start, "endDate": end })
+        instance.post('/adminAvailabilityReport', { "resID":restaurant, "startDate":startString, "endDate": endString })
             .then(function (response) {
                 console.log("raw response:", response);
                 let status = response.data.statusCode;
@@ -48,22 +48,34 @@ export default function Home() {
 
   const handleGenerate = (and:any) => {
     and.preventDefault();
+
+    const today = new Date(currentDateString)
+    const startDate = new Date(startString)
+    const endDate = new Date(endString)
+
     if (restaurant == '') {
       alert("Please enter the restaurant ID.")
     }
-    // date format: YYYY-MM-DD
-    // TO DO: make sure start date is not in the future
-    if (start == '') {
+
+    if (startString == '') {
       alert("Please input a start date for the report.")
     }
-    // TO DO: make sure end date is not in the future & not before the start date
-    if (end == '') {
+    if (startDate >= today) {
+      alert("Start date must be in the past.")
+    }
+
+    if (endString == '') {
       alert("Please input an end date for the report.")
+    }
+    if (endDate >= today) {
+      alert("End date must be in the past.")
+    }
+    if (endDate < startDate) {
+      alert("End date cannot be before the start date.")
     }
     else {
       generateReport()
     }
-    andRefreshDisplay()
   }
 
   // below is where the GUI for the admin generate availability report is drawn
@@ -77,13 +89,14 @@ export default function Home() {
         <br></br>
         <br></br>
         <label className="dateFormat">{"Date Format: YYYY-MM-DD"}</label>
+        <label className="dateMessage">{"If the month or day is only one digit, just enter that digit. (i.e., for March 1, 2025 input 2025-3-1 NOT 2025-03-01)"}</label>
         <br></br>
         <label className="label" htmlFor="start">Start Date:</label>
-        <input type="text" style={{ color: 'black' }} id="start" name="start" value={start} onChange={(and) => setStart(and.target.value)}/>
+        <input type="text" style={{ color: 'black' }} id="start" name="start" value={startString} onChange={(and) => setStartString(and.target.value)}/>
         <br></br>
         <br></br>
         <label className="label" htmlFor="end">End Date:</label>
-        <input type="text" style={{ color: 'black' }} id="end" name="end" value={end} onChange={(and) => setEnd(and.target.value)}/>
+        <input type="text" style={{ color: 'black' }} id="end" name="end" value={endString} onChange={(and) => setEndString(and.target.value)}/>
         <button type="submit" className="generateReportButton">Generate</button>
       </form>
     </div>
